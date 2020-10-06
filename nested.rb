d11cc81d0e7981a4599e062fb9e55e406b611a16
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-06 18:51:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,9 +107,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash[:alan_kay][:known_for] = alans_new_info
+alans_new_info = "GUI"
 programmer_hash
-
 end
 
 def adding_to_dennis
